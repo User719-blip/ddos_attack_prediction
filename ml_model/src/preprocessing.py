236bde926a73ac950a97d825c734c083f19d5545
Subham-Joshi <sj81official@gmail.com>
2025-08-27 22:01:25 +0530

    did some changes updated readme for refrernce
    still working on model training because i wish to implement neural network which is new for me so i need to first study throughly

--- ml_model/src/preprocessing.py
+++ ml_model/src/preprocessing.py
@@ -2,19 +2,33 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from loggger import get_logger
 class PreProcessedData:
-    @staticmethod
-    def pre_process():
-        logg= get_logger(__name__)
-        try:
-            data = pd.read_parquet("ml_model//data//combined_dataset.parquet")
-            data = data.dropna()
-            label= data["Label"].unique()
-            nLabel= data["Label"].nunique()
-            data=data.drop(columns=["Flow ID1", "Src IP", "Dst IP", "Src Port", "Dst Port", "Timestamp"],errors='ignore')
-            print(data.columns)
-            logg.info("helllo2")
-            return data
-        except Exception as e:
-            logg.error("An error occurred",e)
-pre=PreProcessedData.pre_process()
+    def __init__(self):
+        self.data = None
+        self.columns_data = None
+        self.labels = None
+        self.n_labels = None
+        self.logg = get_logger(__name__)
 
+    def pre_process(self):   
+        try:
+            # data reading and droping null values
+            self.data = pd.read_parquet("ml_model//data//combined_dataset.parquet")
+            self.data = self.data.dropna()
+            # attributes
+            self.labels = self.data["Label"].unique()
+            self.n_labels = self.data["Label"].nunique()
+            self.columns_data = self.data.columns
+            #data drop columns
+            self.data = self.data.drop(
+                columns=["Flow ID1", "Src IP", "Dst IP", "Src Port", "Dst Port", "Timestamp"],
+                errors="ignore"
+            )
+
+            #logging
+            self.logg.info("Preprocessing completed successfully.")
+            print("Logged -Preprocessing completed successfully.")
+            return self.data
+
+        except Exception as e:
+            self.logg.error("An error occurred while preprocessing the data: %s", e)
+            return None
